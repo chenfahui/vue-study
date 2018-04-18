f3f38ad3f70eb518b6489c7ab46a932fb6a2cba7
chenfahui <chenfahui@lexiangbao.com>
2018-04-18 18:16:08 +0800

    vue 练习项目

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <hello/>
+  </div>
+</template>
+
+<script>
+import hello from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      message :'ccc'
+    }
+  },
+  components: {
+    hello
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  <input v-model="msg">
+  <button v-on:click="reverseMessage">逆转消息</button>
+  <p v-if="seen">你看到我了</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your App',
+      seen : true
+    }
+  },
+  methods:{
+    reverseMessage:function(){
+      this.msg = this.msg.split('').reverse().join('');
+      this.seen = false;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
