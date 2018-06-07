b655ecfd833948b7e7145feb1d6a5653b6eaff82
chenfahui <chenfahui@lexiangbao.com>
2018-06-07 21:08:01 +0800

    从零开始简单的路由

--- src/main.js
+++ src/main.js
@@ -5,9 +5,29 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+const NotFound = {template:'<p>Page not found</p>'}
+const Home = {template:'<p>Home page<a href="../about">about</a></p>'}
+const About = {template:'<p>About page<a href="../home">home</a></p>'}
+
+const routes = {
+    '/home':Home,
+    '/about':About
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  data:{
+    currentRoute:window.location.pathname
+  },
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  computed:{
+    ViewComponent(){
+        return routes[this.currentRoute] || App;
+    }
+  },
+  render(h){
+    return h(this.ViewComponent)
+  }
 })
